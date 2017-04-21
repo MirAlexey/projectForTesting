e54c71c3efbe5d30f9b914a1b23e69a89a2a1e2c
MirAlexey <trigmir@yandex.ru>
2017-04-21 15:29:06 +0300

    Пробуем простейшие функции slim'а

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App();
+
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('views', [
+        'cache' => 'cache'
+    ]);
+    
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
+
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->view->render($response, 'profile.html', [
+        'name' => $args['name']
+    ]);
+})->setName('profile');
+
+$app->get('/', function (Request $request, Response $response) {
+    
+    return $this->view->render($response, 'start.html', [
+        'name' => "Alex"
+    ]);
+});
+
+$app->run();
+
+
